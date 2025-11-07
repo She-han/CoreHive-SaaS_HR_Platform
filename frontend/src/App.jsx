620f0200851c4cfad2cd1a2041545b0f18aa964a
Shehan Garusingarachchige <shehangarusinghe@gmail.com>
2025-11-08 02:43:54 +0530

    frontend initialized with tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-purple-600'>Welcome to CoreHive SaaS HR Platform</h1>
+    </div>
+  )
+}
+export default App

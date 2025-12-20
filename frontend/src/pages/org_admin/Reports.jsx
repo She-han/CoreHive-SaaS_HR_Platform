c2f82d75ce01c1ecc19ddfd1032fe703bb6721d5
Shehan Garusingarachchige <shehangarusinghe@gmail.com>
2025-12-20 15:05:54 +0530

    create new pages for org_admin

---
+++ frontend/src/pages/org_admin/Reports.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DashboardLayout from '../../components/layout/DashboardLayout'
+
+const Reports = () => {
+  return (
+    <DashboardLayout>Reports</DashboardLayout>
+  )
+}
+
+export default Reports

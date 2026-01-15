5bdcc17f26743031cdd484360e98d0e2456285f4
KmAC171 <kmcmadushan2002@gmail.com>
2026-01-15 18:12:30 +0530

    AuditActivitiEvent finished

--- backend/src/main/java/com/corehive/backend/auditlogs/AuditActivitiEvent.java
+++ backend/src/main/java/com/corehive/backend/auditlogs/AuditActivitiEvent.java
@@ -1,4 +1,26 @@
 package com.corehive.backend.auditlogs;
 
-public class AuditActivitiEvent {
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+public class AuditActivitiEvent extends ApplicationEvent {
+    private final String userEmail;
+    private final String role;
+    private final String action;
+    private final String resource;
+    private final String details;
+    private final String severity;
+    private final String ip;
+
+    public AuditActivitiEvent(Object source, String userEmail, String role, String action, String resource, String details, String severity, String ip) {
+        super(source);
+        this.userEmail = userEmail;
+        this.role = role;
+        this.action = action;
+        this.resource = resource;
+        this.details = details;
+        this.severity = severity;
+        this.ip = ip;
+    }
 }

6640352515c23f30ba458b8188c045e231b9370d
Imasha <imashaudayanganie2020@gmail.com>
2026-01-17 16:15:20 +0530

    CRUD of notice is completed

--- backend/src/main/java/com/corehive/backend/controller/NoticeController.java
+++ backend/src/main/java/com/corehive/backend/controller/NoticeController.java
@@ -11,7 +11,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/orgs/notices")
+@RequestMapping("/api/notices")
 @CrossOrigin(origins = "*")
 public class NoticeController {
 
